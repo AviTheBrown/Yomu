04af148f375d368aa4e7e85ffefde8ff0173b9c6
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2026-02-16 00:21:40 -0500

    Implement TUI loop boilerplate in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-pub fn main() {
-    println!("test")
+use crossterm::{
+    ExecutableCommand,
+    event::KeyCode,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::prelude::*;
+use std::io::stdout;
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    loop {
+        terminal.draw(|frame| {
+            // TODO ui
+        })?;
+        if crossterm::event::poll(std::time::Duration::from_millis(100))? {
+            if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
+                if key.code == crossterm::event::KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
 }
